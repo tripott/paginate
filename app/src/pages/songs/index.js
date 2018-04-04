243c6b33e0d9cb72c52bce6cc1b145525b4d3568
tripott <tripott@gmail.com>
2018-04-04 12:16:25 -0400

    demo setup for page

--- app/src/pages/songs/index.js
+++ app/src/pages/songs/index.js
@@ -2,14 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { getSongs } from '../../action-creators/songs'
 import SongListItem from '../../components/SongListItem'
-import PageNav from '../../components/PageNav'
+
 import List from 'material-ui/List'
 import { CircularProgress } from 'material-ui/Progress'
-import { map, find } from 'ramda'
-import {
-  INCREMENT_SONG_PAGE_COUNTER,
-  DECREMENT_SONG_PAGE_COUNTER
-} from '../../constants'
+import { map } from 'ramda'
 
 class Songs extends React.Component {
   componentDidMount() {
@@ -23,10 +19,7 @@ class Songs extends React.Component {
     }
   }
   render() {
-    const { songs, page, navPrev, navNext, pageHistory } = this.props
-    //use limit from the app url to state your page size
-    const params = new URLSearchParams(this.props.location.search)
-    const limit = params.get('limit') || 3
+    const { songs } = this.props
 
     if (this.props.songLoadStatus === 'loading') {
       return <CircularProgress color="secondary" />
@@ -38,14 +31,6 @@ class Songs extends React.Component {
           <List>
             {map(song => <SongListItem key={song._id} song={song} />, songs)}
           </List>
-          <PageNav
-            {...this.props}
-            page={page}
-            pageCount={songs.length}
-            navPrev={navPrev(pageHistory, page, limit)}
-            navNext={navNext(pageHistory, page, limit)}
-            limit={limit}
-          />
         </div>
       )
     }
@@ -56,31 +41,14 @@ const mapStateToProps = state => {
   return {
     songLoadStatus: state.songLoadStatus,
     songs: state.songs,
-    page: state.songPageCounter,
-    pageHistory: state.pageHistory
+    page: state.songPageCounter
   }
 }
 
 const mapActionsToProps = dispatch => {
   return {
     getSongs: (limit, startKey, page) =>
-      dispatch(getSongs(limit, startKey, page)),
-    navNext: (pageHistory, page, limit) => () => {
-      page = page + 1
-      const pageInHistory = find(ph => ph.page === page - 1, pageHistory)
-      if (pageInHistory) {
-        dispatch(getSongs(limit, pageInHistory.next, page))
-      }
-      dispatch({ type: INCREMENT_SONG_PAGE_COUNTER })
-    },
-    navPrev: (pageHistory, page, limit) => () => {
-      page = page - 1
-      const pageInHistory = find(ph => ph.page === page, pageHistory)
-      if (pageInHistory) {
-        dispatch(getSongs(limit, pageInHistory.current, page))
-      }
-      dispatch({ type: DECREMENT_SONG_PAGE_COUNTER })
-    }
+      dispatch(getSongs(limit, startKey, page))
   }
 }
 
